e7edd09e3965ad39b2d52d2fff340b9dd20d0092
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-12-09 22:00:03 +0000

    Added working auth route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import app from './app';
+import auth from './routes/auth';
+
+app.use('/auth', auth);
 
 app.listen({port: 8000}, () => {
   console.log('*** STARTED AUTH MICROSERVICE ***');
---
+++ src/routes/auth.ts
@@ -0,0 +1,13 @@
+import Router from 'express-promise-router';
+
+const router = Router();
+
+router.get('/login', (req, res) => {
+  res.send("login");
+});
+
+router.get('/register', (req, res) => {
+  res.send("register");
+});
+
+export default router;
